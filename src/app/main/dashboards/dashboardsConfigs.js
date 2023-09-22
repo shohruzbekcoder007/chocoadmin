da1b2670a678f9d557ea8eb9b13c10cb2c6217e9
shohruzbekcoder <shohruzbekcoder007@gmail.com>
2023-09-22 17:17:34 +0500

    add a link and components1

--- src/app/main/dashboards/dashboardsConfigs.js
+++ src/app/main/dashboards/dashboardsConfigs.js
@@ -1,13 +1,13 @@
 import AnalyticsDashboardAppConfig from './analytics/AnalyticsDashboardAppConfig';
 import ProjectDashboardAppConfig from './project/ProjectDashboardAppConfig';
 import FinanceDashboardAppConfig from './finance/FinanceDashboardAppConfig';
-import CryptoDashboardAppConfig from './crypto/CryptoDashboardAppConfig';
+// import CryptoDashboardAppConfig from './crypto/CryptoDashboardAppConfig';
 
 const dashboardsConfigs = [
   AnalyticsDashboardAppConfig,
   ProjectDashboardAppConfig,
   FinanceDashboardAppConfig,
-  CryptoDashboardAppConfig,
+  // CryptoDashboardAppConfig,
 ];
 
 export default dashboardsConfigs;
